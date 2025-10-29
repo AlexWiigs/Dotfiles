85f9d9f045e0ddfa1721369e013737895faa78f7
AlexWiigs <alexwiigs@gmail.com>
2025-10-29 13:31:18 -0600

    Added support for quarto files.

---
+++ nvim/lua/plugins/quarto.lua
@@ -0,0 +1,22 @@
+return {
+  "quarto-dev/quarto-nvim",
+  dependencies = {
+    "jmbuhr/otter.nvim",      -- lets LSPs attach to code blocks
+    "nvim-treesitter/nvim-treesitter",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    require("quarto").setup({
+      lspFeatures = {
+        languages = { "python", "r", "lua", "bash" },
+        chunks = "all",
+        diagnostics = {
+          enabled = true,
+          triggers = { "BufWritePost" },
+        },
+        completion = { enabled = true },
+      },
+    })
+  end,
+}
